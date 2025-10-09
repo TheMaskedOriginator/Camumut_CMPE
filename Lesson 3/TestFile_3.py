7184185cbdf2645f29ad9bbad17edb55b2ac7190
TheMaskedOriginator <kirkclaret1102@gmail.com>
2025-10-09 09:30:19 +0800

    all

---
+++ Lesson 3/TestFile_3.py
@@ -0,0 +1,9 @@
+print("Conversation")
+
+input("Enter Password: ")
+print("Z: Hey Kayla")
+print("Kayla: Hello, Z! I would like you to meet my friend")
+name = input("Enter Your Name: ")
+print("Z: why are you doing this...")
+print("Kayla: please... stop this!!!")
+print("Z: oh, it is nice to meet you " + name)

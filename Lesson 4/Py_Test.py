9a5752e66493327cca5795dff6047dbd6aa76edf
TheMaskedOriginator <kirkclaret1102@gmail.com>
2025-10-20 20:43:34 +0800

    Lesson 4/InClass_Tests.py

---
+++ Lesson 4/Py_Test.py
@@ -0,0 +1,24 @@
+strChar1 = "Pomni"
+strChar1nn = "Pompom"
+strChar2 = "Ragatha"
+strChar2nn = "Ragdoll"
+strChar3 = "Jax"
+
+import time
+print("Jax: oh, hey " + strChar1nn + ". What'ya have there?")
+time.sleep(1)
+
+print("Ragatha: wha-, " + strChar3 + "!?")
+time.sleep(.5)
+
+print("Jax: oh, seems like " + strChar2nn + " is also here.")
+time.sleep(1)
+
+print("Ragatha: I told you to stop calling me that!")
+time.sleep(1.2)
+
+print("Jax: eh. I can't remember anything that comes out from your mouth, " + strChar2nn)
+time.sleep(1)
+
+print("Jax: Besides! Im only here for " + strChar1nn + " only, so shoo now.")
+time.sleep(1)
